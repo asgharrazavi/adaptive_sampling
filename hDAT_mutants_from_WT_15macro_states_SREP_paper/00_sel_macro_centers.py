546256055624d38290d2cdb990c1376cb959e813
Asghar Razavi <asghar.razavi@gmail.com>
2019-07-10 08:24:53 -0400

    00

--- hDAT_mutants_from_WT_15macro_states_SREP_paper/00_sel_macro_centers.py
+++ hDAT_mutants_from_WT_15macro_states_SREP_paper/00_sel_macro_centers.py
@@ -2,12 +2,11 @@ import numpy as np
 import mdtraj as md
 
 
-#ref = md.load('/Users/asr2031/Desktop/transfer/dDAT_WT_ensemble_carver/ionized.pdb')
 ref = md.load('/Users/asr2031/Desktop/transfer/dDAT_WT_ensemble_carver/ionized.pdb')
 
-for i in [0]:
-#    t = md.load('/Users/asr2031/Desktop/transfer/dDAT_WT_ensemble_carver/msm/tica_l16ns_new_pars12_2/projected/16ns/100micros/15macros/frames_for_macros/small_frames/macro_%d.xtc' %i, top=ref)
-    ind = 76 #np.random.choice(range(t.xyz.shape[0]),1)
+for i in range(15):
+    t = md.load('/Users/asr2031/Desktop/transfer/dDAT_WT_ensemble_carver/msm/tica_l16ns_new_pars12_2/projected/16ns/100micros/15macros/frames_for_macros/small_frames/macro_%d.xtc' %i, top=ref)
+    ind = np.random.choice(range(t.xyz.shape[0]),1)
     print i, t.xyz.shape, ind
     ref.xyz = t.xyz[ind,:,:]
     ref.save_pdb('center_macro_%d.pdb' %i)
