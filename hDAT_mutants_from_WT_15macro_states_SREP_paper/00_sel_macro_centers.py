ddd10ae7fd36cff9cdcdbf1c6ac3244775f6bde6
Asghar Razavi <asghar.razavi@gmail.com>
2019-07-10 08:26:22 -0400

    00_sel_macro_centers.py

--- hDAT_mutants_from_WT_15macro_states_SREP_paper/00_sel_macro_centers.py
+++ hDAT_mutants_from_WT_15macro_states_SREP_paper/00_sel_macro_centers.py
@@ -7,7 +7,7 @@ ref = md.load('/Users/asr2031/Desktop/transfer/dDAT_WT_ensemble_carver/ionized.p
 for i in range(15):
     t = md.load('/Users/asr2031/Desktop/transfer/dDAT_WT_ensemble_carver/msm/tica_l16ns_new_pars12_2/projected/16ns/100micros/15macros/frames_for_macros/small_frames/macro_%d.xtc' %i, top=ref)
     ind = np.random.choice(range(t.xyz.shape[0]),1)
-    print i, t.xyz.shape, ind
+    print "macro_id, xyz.shape, selected frame id:", i, t.xyz.shape, ind
     ref.xyz = t.xyz[ind,:,:]
     ref.save_pdb('center_macro_%d.pdb' %i)
 
